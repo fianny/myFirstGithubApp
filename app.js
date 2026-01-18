7568b41016d19fa50b122a98184ac697a34041e8
江菲兒 <fianny@jiangfeiers-MacBook-Pro.local>
2026-01-18 17:30:39 +0800

    add codes

---
+++ app.js
@@ -0,0 +1,84 @@
+import dotenv from "dotenv";
+import {App} from "octokit";
+import {createNodeMiddleware} from "@octokit/webhooks";
+import fs from "fs";
+import http from "http";
+import { ppid } from "process";
+
+dotenv.config();
+
+const appId = process.env.APP_ID;
+const webhookSecret = process.env.WEBHOOK_SECRET;
+const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+
+const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+
+const app = new App({
+  appId: appId,
+  privateKey: privateKey,
+  webhooks: {
+    secret: webhookSecret
+  },
+});
+
+const messageForNewPRs = "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
+
+const messageForPushes = "Thanks for pushing changes! Please ensure all tests pass before merging.";
+
+async function handlePullRequestOpened({octokit, payload}) {
+  console.log(`Received a pull request event for #${payload.pull_request.number}`);
+
+  try {
+    await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
+      owner: payload.repository.owner.login,
+      repo: payload.repository.name,
+      issue_number: payload.pull_request.number,
+      body: messageForNewPRs,
+      headers: {
+        "x-github-api-version": "2022-11-28",
+      },
+    });
+  } catch (error) {
+    if (error.response) {
+      console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+    }
+    console.error(error)
+  }
+};
+
+async function handlePush({octokit, payload}) {
+  console.log(`Received a push event for ${payload.repository.full_name} to ${payload.ref}`);
+
+}
+async function handleCommitCommentCreated({octokit, payload}) {
+  console.log(`Received a commit comment event for ${payload.repository.full_name} to ${payload.ref}`);
+
+}
+
+app.webhooks.on("pull_request.opened", handlePullRequestOpened);
+app.webhooks.on("push", handlePush);
+app.webhooks.on("commit_comment.created" , handleCommitCommentCreated);
+
+
+app.webhooks.onError((error) => {
+  if (error.name === "AggregateError") {
+    console.error(`Error processing request: ${error.event}`);
+  } else {
+    console.error(error);
+  }
+});
+
+const port = 3000;
+const host = 'localhost';
+const path = "/api/webhook";
+const localWebhookUrl = `http://${host}:${port}${path}`;
+
+const middleware = createNodeMiddleware(app.webhooks, { path });
+
+http.createServer(middleware).listen(port, () => {
+  console.log(`Server is listening for events at: ${localWebhookUrl}`);
+  console.log('Press Ctrl + C to quit.')
+});
+
+
+
